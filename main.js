cbf5328e8dc11ce718b2fbf48573759246b183da
DataCentricAI-ICDM <129226520+DataCentricAI-ICDM@users.noreply.github.com>
2023-03-28 17:33:11 -0400

    Initialization of DataCentric AI Workshop

---
+++ main.js
@@ -0,0 +1,3 @@
+document.getElementById("registerBtn").addEventListener("click", function () {
+    window.location.href = "https://www.example.com/registration"; // Replace with the registration link.
+});
