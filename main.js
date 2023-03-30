49d691ea3255e96ec1199e0779e18b0b3444c703
DataCentricAI-ICDM <129226520+DataCentricAI-ICDM@users.noreply.github.com>
2023-03-30 15:56:35 +0000

    replace new page

--- main.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("registerBtn").addEventListener("click", function () {
-    window.location.href = "https://www.example.com/registration"; // Replace with the registration link.
-});
